69730611cf316f7a7e7442aba4209e23d78e5c48
Autumn-Davis <autumn.davis@colorado.edu>
2024-11-05 10:46:24 -0700

    ica 11

---
+++ js/ica11.js
@@ -0,0 +1,20 @@
+let numChildren1 = 1;
+let numChildren2 = 2;
+let numChildren3 = 3;
+
+let partName1 = "Dave";
+let partName2 = "Stewart";
+let partName3 = "Bob";
+
+let geoLoc1 = "Denver CO";
+let geoLoc2 = "Copenhagen DK";
+let geoLoc3 = "Mexico City, MX";
+
+let job1 = "DIY tiny home content creator";
+let job2 = "Emergency room surgeon";
+let job3 = "Gynocologist"
+
+
+function tellFortune(numberChildren, partnerName, geographicLocation, job title){
+    return(
+}
