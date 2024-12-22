db6104d37c0079e0add1e0fff30ab280dc5ec2b5
Autumn-Davis <autumn.davis@colorado.edu>
2024-12-22 09:37:29 -0700

    ICA 11 stuff

--- js/ica11.js
+++ js/ica11.js
@@ -1,20 +1,39 @@
-let numChildren1 = 1;
-let numChildren2 = 2;
-let numChildren3 = 3;
+const story = document.querySelector('.fortune');
 
-let partName1 = "Dave";
-let partName2 = "Stewart";
-let partName3 = "Bob";
-
-let geoLoc1 = "Denver CO";
-let geoLoc2 = "Copenhagen DK";
-let geoLoc3 = "Mexico City, MX";
-
-let job1 = "DIY tiny home content creator";
-let job2 = "Emergency room surgeon";
-let job3 = "Gynocologist"
+let numChildren = ["1", "2", "3"];
+let partName = ["Nibbles McSqueakerton", "Froderick Hoppington", "Pip Fluffwing"];
+let location = ["the Moon", "your mom's pantry", "Alpha Centauri"];
+let job = ["DIY tiny home content creator", "Emergency room surgeon", "Gynocologist"];
 
 
-function tellFortune(numberChildren, partnerName, geographicLocation, job title){
-    return(
+
+var children = randomValueFromArray(numChildren);
+var name = randomValueFromArray(partName);
+var home = randomValueFromArray(location);
+var occupation = randomValueFromArray(job);
+
+document.getElementById("button").addEventListener("click", function(e){
+    console.log("Pressed");
+    alert("test");
+    fortune(children, name, home, occupation);
+})
+
+function randomValueFromArray(array){
+    const random = Math.floor(Math.random()*array.length);
+    return array[random];
+}
+
+function fortune(input1, input2, input3, input4){
+    document.getElementById("paragraph").innerHTML = "new text";
+    console.log("test");
+
+    let newStory = "You will be a X in Y, and married to Z with N kids.";
+
+    newStory = newStory.replace('X', randomValueFromArray(input4));
+    newStory = newStory.replace('Y', randomValueFromArray(input3));
+    newStory = newStory.replace('Z', randomValueFromArray(input2));
+    newStory = newStory.replace('N', randomValueFromArray(input1));
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
 }
